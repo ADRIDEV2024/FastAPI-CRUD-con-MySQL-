51a629a8a2df9a3d97f5b2053000ed66274f8100
Adrian <adriangomis34@gmail.com>
2024-10-08 13:35:00 +0200

    Update employee_schema.py

--- Schema/employee_schema.py
+++ Schema/employee_schema.py
@@ -1,12 +1,1 @@
-from pydantic import BaseModel
-from typing import Optional
 
-class EmployeeSchema(BaseModel):
-    id: Optional[str]
-    name: str
-    password: str
-    age: int
-    department: str
-    position: str
-    years_worked: str
-    salary_month: int
