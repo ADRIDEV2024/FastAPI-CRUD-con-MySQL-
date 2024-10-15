682e6c2ccd67e7ba7a0b14caedb171b85a24f3dd
Adrian <adriangomis34@gmail.com>
2024-10-15 10:17:10 +0200

    Update oauth_security.py

--- Oauth/oauth_security.py
+++ Oauth/oauth_security.py
@@ -31,7 +31,7 @@ employees_db = {
 }
 
 # Password hashing
-pwd_context = CryptContext(schemes=["pkbdf2"], deprecated="auto")
+pwd_context = CryptContext(schemes=["pkbdf2"], deprecated=false)
 
 class Token(BaseModel):
     access_token: str
