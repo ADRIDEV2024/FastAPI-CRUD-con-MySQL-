ed6e9e28132b42136c2e9d2310d9be13763a0fd2
Adrian <adriangomis34@gmail.com>
2024-11-26 09:15:30 +0100

    Update oauth_security.py

--- oauth_security.py
+++ oauth_security.py
@@ -4,9 +4,7 @@ from configs import MY_SECRET_KEY
 
 from fastapi import Depends, HTTPException
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
-from jose import JWTError, jwt
-from passlib.context import CryptContext
-from pydantic import BaseModel
+
 
 
 SECRET_KEY = MY_SECRET_KEY
