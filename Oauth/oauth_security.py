28a6b12b0dc3a07661a03e2366795e049f7503f5
Adrian <adriangomis34@gmail.com>
2024-12-10 08:36:01 +0100

    Update oauth_security.py

--- Oauth/oauth_security.py
+++ Oauth/oauth_security.py
@@ -22,7 +22,7 @@ app = FastAPI()
 employees_db = {
     "johndoe": {
         "id": 2,
-        "name": "Jane",
+        "name": "John",
         "department": "logistics",
         "email": "johndoe@example.com",
         "hashed_password": "$2b$12$EixZaYVK1fsbw1ZfbX3OXePaWxn96p36WQoeG6Lruj3vjPGga31lW",
