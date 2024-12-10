f1bdca6aee00c6e415928fe46886ef23fb4d6bb5
Adrian <adriangomis34@gmail.com>
2024-12-10 08:30:58 +0100

    Update db.py

--- Config/db.py
+++ Config/db.py
@@ -4,4 +4,5 @@ from sqlalchemy import create_engine, MetaData
 engine = create_engine("mysql+pymysql://adridev:633900@localhost:3300/db-employees")
 
 conn = engine.connect()
+
 metadata = MetaData()
